a043615c60f109f567f9b96979904b116cd8eaba
OpKSpatY <mcpedrohenriquelc@gmail.com>
2026-03-26 13:36:44 -0300

    feat: added .env swagger verification

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,15 +16,19 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttpExceptionFilter());
-  const config = new DocumentBuilder()
-    .setTitle('Cookingtop API')
-    .setDescription('Documentação da API do Cookingtop')
-    .setVersion('1.0')
-    .addTag('cookingtop')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
+
+  if (configService.get<string>('NODE_ENV') === 'development') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('Cookingtop API')
+      .setDescription('Documentação da API do Cookingtop')
+      .setVersion('1.0')
+      .addTag('cookingtop')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api', app, document);
+  }
+
   const port = process.env.PORT ?? 3000;
   try {
     await app.listen(port, '0.0.0.0');
